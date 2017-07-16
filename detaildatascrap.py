9618ba1e24bd7797744ec6e098587099c49a53c8
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2017-07-17 02:04:54 +0800

    Update detaildatascrap.py

--- detaildatascrap.py
+++ detaildatascrap.py
@@ -30,8 +30,8 @@ soup = BeautifulSoup(r.text)
 action_url = soup.find('form', id='login_form')['action']
 inputs = soup.find('form', id='login_form').findAll('input', {'type': ['hidden', 'submit']})
 post_data = {input.get('name'): input.get('value')  for input in inputs}
-post_data['email'] = 'xrickliao@gmail.com'
-post_data['pass'] = 'rlpwd20270528'.upper()
+post_data['email'] = 'account'
+post_data['pass'] = 'password'.upper()
 scripts = soup.findAll('script')
 scripts_string = '/n/'.join([script.text for script in scripts])
 datr_search = re.search('\["_js_datr","([^"]*)"', scripts_string, re.DOTALL)
